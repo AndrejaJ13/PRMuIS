0b074839f6a03415fc761b61e2741cf800d668b3
AndrejaJ13 <andreja.jovanic13@gmail.com>
2025-08-31 23:05:12 +0200

    promene za osmu tacku

--- Parking/Domain/Zauzece.cs
+++ Parking/Domain/Zauzece.cs
@@ -1,11 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
 
-[Serializable]
-public class Zauzece
+namespace Domain
 {
-    public int BrojParkinga { get; set; }
-    public int BrojMesta { get; set; }
-    public string VremeNapustanja { get; set; }
+    [Serializable]
+    public class Zauzece
+    {
+        public int BrojParkinga { get; set; }
+        public int BrojMesta { get; set; }
+        public string VremeNapustanja { get; set; }
+        public DateTime VremeDolaska { get; set; } = DateTime.Now;
+        public List<Car> Cars { get; set; } = new List<Car>();
 
-    public DateTime VremeDolaska { get; set; } = DateTime.Now;
+        public override string ToString()
+        {
+            var baseString = $"Parking: {BrojParkinga}\nBroj mesta: {BrojMesta}\nVreme napustanja: {VremeNapustanja}\nVreme dolaska: {VremeDolaska}";
+
+            if (Cars != null && Cars.Any())
+            {
+                baseString += "\nVozila:";
+                foreach (var car in Cars)
+                {
+                    baseString += $"\n  {car}";
+                }
+            }
+
+            return baseString;
+        }
+    }
 }
